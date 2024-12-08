ecfc02d6d495c012d0fa7cfe5a88099aaa03a367
HSarfaraz <message.sarfarazhussain@yahoo.com>
2024-12-08 19:18:14 +0530

    add the initial setup frontend backend with sample json

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # GFG-FoodDelivery
+
+Dec 2024:
+
+# Mongodb credentials:
+
+login id: messagesarfarazhussain@yahoo.com
+
+username: messagesarfarazhussain
+
+password: XOKfSqRHcAZ3S7eO
+
+steps: create the db --> connect to drivers --> save the connection string
+
+# connection string:
+
+mongodb+srv://messagesarfarazhussain:XOKfSqRHcAZ3S7eO@gfg-mern.hlb4k.mongodb.net/?retryWrites=true&w=majority&appName=GFG-MERN
+
+steps to access from anywhere:
+root --> network access --> add ip --> allow access from anywhere --> confirm
+
+github url: https://github.com/geekanant/fsnrl-53
+
+for cloning: https://github.com/HSarfaraz/GFG-FoodDelivery.github
+
+GFG-FoodDelivery
+
+# Database setup population:
+
+insert document --> upload the dishes.json inside {}
---
+++ backend/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+const dishes = require("./routes/dishRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+app.use(express.json());
+
+mongoose
+    .connect(
+      "mongodb+srv://messagesarfarazhussain:XOKfSqRHcAZ3S7eO@gfg-mern.hlb4k.mongodb.net/?retryWrites=true&w=majority&appName=GFG-MERN"
+    )
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// middleware
+app.use((req, res, next) => {
+  console.log("Time:", Date.now());
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json("Hello");
+});
+
+app.use("/api", dishes);
+app.use("/api/user", userRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
